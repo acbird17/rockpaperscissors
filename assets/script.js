671d3aded11069f7031de6f03d2f81efca0ea2f9
Abird <acbird.ab@gmail.com>
2022-05-04 20:18:24 -0600

    working ifs

--- assets/script.js
+++ assets/script.js
@@ -3,32 +3,28 @@ var outcomes = ["r", "p", "s"];
 userInput = prompt("Input r for Rock, p for Paper, s for Scissors");
 console.log(userInput);
 
-var r = 0;
-var p = 1;
-var s = 2;
-
 var randomIndex = Math.floor(Math.random() * 3);
 
-var computerChoice = outcomes[randomIndex];
+var computerChoice = outcomes[randomIndex].toLowerCase();
 console.log(computerChoice);
 
-if (userInput === r && computerChoice === s) {
+if (userInput === "r" && computerChoice === "s") {
   alert("Computer chose Scissors, you win!");
-} else if (computerChoice === r && userInput === s) {
+} else if (computerChoice === "r" && userInput === "s") {
   alert("Computer chose Rock, you lose.");
-} else if (userInput === p && computerChoice === r) {
+} else if (userInput === "p" && computerChoice === "r") {
   alert("Computer chose Rock, you win!");
-} else if (computerChoice === p && userInput === r) {
+} else if (computerChoice === "p" && userInput === "r") {
   alert("Computer chose Paper, you lose.");
-} else if (userInput === s && computerChoice === p) {
+} else if (userInput === "s" && computerChoice === "p") {
   alert("Computer chose Paper, you win!");
-} else if (computerChoice === s && userInput === p) {
+} else if (computerChoice === "s" && userInput === "p") {
   alert("Computer chose Scissors, you lose.");
-} else if (userInput === r && computerChoice === r) {
+} else if (userInput === "r" && computerChoice === "r") {
   alert("Computer chose Rock, it's a draw.");
-} else if (userInput === p && computerChoice === p) {
+} else if (userInput === "p" && computerChoice === "p") {
   alert("Computer chose Paper, it's a draw.");
-} else if (userInput === s && computerChoice === s) {
+} else if (userInput === "s" && computerChoice === "s") {
   alert("Computer chose Scissors, it's a draw.");
 } else {
   alert("Invalid input, you lose.");
